fbf608a7834e44da0509dcf1e548f2abcdba6771
Giampiero015 <150815405+Giampiero015@users.noreply.github.com>
2023-12-21 10:07:41 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# promise3
--Crea una semplice Promise che si risolve dopo un ritardo di 2 secondi. 
--Utilizzare setTimeout per simulare un'operazione asincrona.
+# async3
+In questo esercizio, come nel precendente andremo a simulare il caricamento di moduli di un veicolo spaziale in modo asincrono e quindi lanciare il veicolo spaziale quando tutti i moduli sono stati caricati con successo!
+
+ATTENZIONE : il caricamento dei moduli in questo esercizio non sempre potrebbe riuscire!
+
+Implementa la funzione asincrona "lanciaVeicoloSpaziale" che chiama in sequenza la funzione "caricaModulo" per caricare i moduli A, B e C, attendendo il completamento di ciascuna operazione prima di passare alla successiva.
+
+Gestisci eventuali errori nel caricamento dei moduli stampando in console "Impossibile lanciare il veicolo spaziale a causa di errori nei moduli."
+
+Alla fine, se tutti i moduli sono stati caricati con successo, stampa in console "Veicolo spaziale pronto per il lancio!".
+
--- es3.js
+++ es3.js
@@ -1,20 +1,24 @@
-function promise(id){
-    
-        return new Promise((resolve,reject)=>{
-            setTimeout(() => {
-                if(id==true)
-                {
-                    resolve ("OK");
-                }
-                else
-                {
-                    reject ("KO");
-                }
-            }, 2000);
-        });
-    
+function caricaModulo(modulo) {
+    return new Promise((resolve, reject) => {
+      const tempoCaricamento = Math.random() * 3000; // Simula il caricamento con un ritardo casuale fino a 3 secondi.
+      setTimeout(() => {
+        if (tempoCaricamento < 1500) {
+          resolve(`Il modulo ${modulo} è stato caricato.`);
+        } else {
+          reject(`Errore durante il caricamento del modulo ${modulo}.`);
+        }
+      }, tempoCaricamento);
+    });
+  }
+  async function lanciaVeicoloSpaziale() {
+    try {
+        console.log(await caricaModulo("modulo A"));
+        console.log(await caricaModulo("Modulo B"));
+        console.log(await caricaModulo("Modulo C"));
+        console.log("Veicolo spaziale pronto per il lancio!");
+    } catch (error) {
+        console.log(error);
+    }
+
 }
-promise(true).then((data)=>{console.log(data)})
-                .catch((error)=>{console.log(error)});
-promise(false).then((data)=>{console.log(data)})
-                .catch((error)=>{console.log(error)});
+lanciaVeicoloSpaziale();
